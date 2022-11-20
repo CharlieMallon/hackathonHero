0f37ff2f1803b1cb90ff2197cfc095cdce463331
DeanDodds <deandodds88@gmail.com>
2022-11-20 11:37:23 +0000

    added data.jswith test data

---
+++ assets/js/data.js
@@ -0,0 +1,56 @@
+const SONGS =[
+    {
+        "name" : "21 Guns",
+        "artist" : "Green Day",
+        "link" : "/assets/audio/level1.mp3",
+        "image" : "/assets/images/21guns.jpeg",
+        "level": "easy",
+        "bpm" : 80, 
+        "Genre" : "Rock",
+    },
+    {
+        "name" : "Enter Sandman",
+        "artist" : "Metallica",
+        "link" : "/assets/audio/level2.mp4",
+        "image" : "assets/images/entersandman.jpeg",
+        "level": "medium",
+        "bpm" : 123,
+        "Genre" : "metal"
+    },
+    {
+        "name" : "Sweet Child Of Mine",
+        "artist" : "Guns and Roses",
+        "image" : "/assets/images/sweetchild.jpeg",
+        "link" : "/assets/audio/level3.mp3",
+        "level": "hard",
+        "bpm" : 125, 
+        "Genre" : "Rock"
+    },
+    {
+        "name" : "21 Guns",
+        "artist" : "Green Day",
+        "link" : "/assets/audio/level1.mp3",
+        "image" : "/assets/images/21guns.jpeg",
+        "level": "easy",
+        "bpm" : 80, 
+        "Genre" : "Rock",
+    },
+    {
+        "name" : "Enter Sandman",
+        "artist" : "Metallica",
+        "link" : "/assets/audio/level2.mp4",
+        "image" : "assets/images/entersandman.jpeg",
+        "level": "medium",
+        "bpm" : 123,
+        "Genre" : "metal"
+    },
+    {
+        "name" : "Sweet Child Of Mine",
+        "artist" : "Guns and Roses",
+        "image" : "/assets/images/sweetchild.jpeg",
+        "link" : "/assets/audio/level3.mp3",
+        "level": "hard",
+        "bpm" : 125, 
+        "Genre" : "Rock"
+    }
+]
